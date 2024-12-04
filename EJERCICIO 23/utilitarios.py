c976b8f5e74e61977dde8e6916de0140f7689611
mateoalba <alumnos@alumnosuio.ute.edu.ec>
2024-12-03 20:20:51 -0500

    tipomulta

--- EJERCICIO 23/utilitarios.py
+++ EJERCICIO 23/utilitarios.py
@@ -2,3 +2,15 @@ def calcularPromedio(val1,val2,val3):
     calculo=val1+val2+val3
     promedio1=calculo/3
     return promedio1
+
+def consultarMulta(tipomulta):
+    if tipomulta == 1:
+        return 10/100*100
+    if tipomulta == 2:
+        return 15/100*100
+    if tipomulta == 3:
+        return 20/100*100
+    if tipomulta == 4:
+        return 30/100*100
+    else:
+        return -1
