17dcb30933a582812f19b49e8eb0ef87818a76bd
Julianrma <julianreveli@gmail.com>
2024-12-03 19:48:27 -0500

    CalcularSubtotal

--- EJERCICIO 23/utilitarios.py
+++ EJERCICIO 23/utilitarios.py
@@ -1,4 +1,16 @@
+
+
 def calcularPromedio(val1,val2,val3):
     calculo=val1+val2+val3
     promedio1=calculo/3
     return promedio1
+
+def calcularSubtotal(PrecioProducto, Cantidad, PorcentajeDescuento):
+    SubtotalSinDescuento = PrecioProducto * Cantidad 
+    Descuento = (SubtotalSinDescuento * PorcentajeDescuento) / 100
+    Subtotal1 = SubtotalSinDescuento - Descuento
+    Subtotal2 = SubtotalSinDescuento - Descuento
+    Subtotal3 = SubtotalSinDescuento - Descuento
+    return Subtotal1, Subtotal2,Subtotal3
+
+
